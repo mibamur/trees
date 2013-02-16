a00807d6ce3ead66db218bb5c430107ef99be903
anton <anton@asgard.(none)>
2013-02-16 14:46:42 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trees::Application.config.secret_token = '04813a11a3a8a115dbebbc505bfe367f5d27e2f698ded8a9008b4714046adad08fc4898ec51f38590a671729480e875228625833b06d72f7ceae8f684b23af7e'
